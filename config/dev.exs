b0ac53c3fda9e85b33d1c388e0a38238f7e4f89a
basedHokage <chriseugene75@gmail.com>
2021-07-27 09:42:18 +0300

    add controllers, routers, sign up, login, landing pages and some styles. Going to avoid this style of committing in the future

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :messengyr, Messengyr.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "Chuchi22#7",
   database: "messengyr_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
--- lib/messengyr_web/controllers/page_controller.ex
+++ lib/messengyr_web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule MessengyrWeb.PageController do
   use MessengyrWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn)
+  end
+
+  def say_hello(conn, _params) do
+    text(conn, "Hello!")
+  end
+
+  def login(conn, _params) do
+    render(conn)
+  end
+
+  def signup(conn, _params) do
+    render(conn)
   end
 end
